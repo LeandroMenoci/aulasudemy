aefaadcc07304576ca4d48ae45eccaa7e29329c9
LeandroMenoci <menoci.leandro@gmail.com>
2021-10-08 22:14:18 -0300

    operadores de comparação

---
+++ Aula21/operadoresdecomparacao.js
@@ -0,0 +1,11 @@
+/*
+Operadores de comparação
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a
+== igualdade (valor) **** não utiliza este
+=== igualdade estrita (valor e tipo)
+!= diferente (valor) **** não utiliza esse
+!== diferente estrita (valor e tipo)
+*/
