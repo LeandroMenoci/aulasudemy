cc3ebf7a91b0e34fa3c81a256249cbf80692a16f
LeandroMenoci <menoci.leandro@gmail.com>
2021-10-09 10:47:14 -0300

    if-else / IMC

---
+++ Aula25/if-else2.js
@@ -0,0 +1,11 @@
+const numero = 12;
+
+if (numero >= 0 && numero <= 5) {
+  console.log('O número está entre 0 e 5.');
+} else if (numero >= 6 && numero <= 8) {
+  console.log('O número está entre 6 e 8.')
+} else if (numero >= 9 && numero <= 11) {
+  console.log('O número está entre 9 e 11.')
+} else {
+  console.log('O número não está entre 0 e 11.')
+}
