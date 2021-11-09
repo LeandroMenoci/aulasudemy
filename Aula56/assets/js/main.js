0ef67d10eba5cb4bfdf188fe1dd0d889559900a2
Leandro Menoci <78942144+LeandroMenoci@users.noreply.github.com>
2021-11-08 23:41:09 -0300

    Exercicio function factory

---
+++ Aula56/assets/js/main.js
@@ -0,0 +1,73 @@
+function criaCalculadora() {
+  return {
+    display: document.querySelector('.display'),
+    inicia() {
+      this.clickBotoes();
+      this.pressionaEnter();
+    },
+
+    pressionaEnter() {
+      this.display.addEventListener('keyup', e => {
+        if (e.keyCode === 13) {
+          this.realizaConta();
+        }
+      })
+    },
+
+    clearDisplay() {
+      this.display.value = "";
+    },
+
+    apagaUm() {
+      this.display.value = this.display.value.slice(0, -1);
+    },
+
+    realizaConta() {
+      let conta = this.display.value;
+
+      try {
+        conta = eval(conta);
+
+        if (!conta) {
+          alert('Conta inválida');
+          return;
+        }
+
+        this.display.value = conta;
+      } catch (e) {
+        alert('Conta inválida')
+      }
+    },
+
+    clickBotoes() {
+      // this = calculadora
+      document.addEventListener('click', (e) => {
+        const el = e.target;
+
+        if (el.classList.contains('btn-num')) {
+          this.btnParaDisplay(el.innerText);
+        }
+
+        if (el.classList.contains('btn-clear')) {
+          this.clearDisplay();
+        }
+
+        if (el.classList.contains('btn-del')) {
+          this.apagaUm();
+        }
+
+        if (el.classList.contains('btn-eq')) {
+          this.realizaConta();
+        }
+      })
+    },
+
+    btnParaDisplay(valor) {
+      this.display.value += valor;
+    }
+
+  }
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia()
