04e40ba0e3ca29d65fc032d97b358088af0a99ec
LeandroMenoci <menoci.leandro@gmail.com>
2021-11-09 23:22:10 -0300

    Function constructor final

--- Aula56/assets/js/main.js
+++ Aula56/assets/js/main.js
@@ -88,14 +88,6 @@ function Calculadora() {
     });
   };
 
-  this.clearDisplay = () => {
-    this.display.value = "";
-  }
-
-  this.apagaUm = () => {
-    this.display.value = this.display.value.slice(0, -1)
-  };
-
   this.realizaConta = () => {
     let conta = this.display.value;
 
@@ -112,31 +104,27 @@ function Calculadora() {
     }
   };
 
+  this.btnParaDisplay = (valor) => {
+    this.display.value += valor;
+    this.display.focus()
+  }
+
+  this.clearDisplay = () => this.display.value = "";
+  this.apagaUm = () => this.display.value = this.display.value.slice(0, -1)
+
+
   this.clickBotoes = () => {
     document.addEventListener('click', (e) => {
       const el = e.target;
 
-      if (el.classList.contains('btn-num')) {
-        this.btnParaDisplay(el.innerText)
-      };
-
-      if (el.classList.contains('btn-clear')) {
-        this.clearDisplay();
-      };
-
-      if (el.classList.contains('btn-del')) {
-        this.apagaUm();
-      };
-
-      if (el.classList.contains('btn-eq')) {
-        this.realizaConta();
-      }
+      if (el.classList.contains('btn-num')) this.btnParaDisplay(el.innerText)
+      if (el.classList.contains('btn-clear')) this.clearDisplay();
+      if (el.classList.contains('btn-del')) this.apagaUm();
+      if (el.classList.contains('btn-eq')) this.realizaConta();
     })
   };
 
-  this.btnParaDisplay = (valor) => {
-    this.display.value += valor;
-  }
+
 }
 
 const calculadora = new Calculadora();
